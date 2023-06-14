a7bd29106a05c9c9f42950311f240f04b9d68040
badnng <wsj316@outlook.com>
2023-06-14 17:16:18 +0800

    Update the Batch(beta6)

--- start(EN).bat
+++ start(EN).bat
@@ -11,47 +11,55 @@ echo.
 echo. Automatic Magisk Delta Flashing
 echo. by badnng
 echo.Press A to start the fully automated flashing of boot for older models~
-echo.Press B to start the fully automated flashing of int_boot for newer models~
+echo.Press B to start the fully automated flashing of init_boot for newer models~
 
-%Check if files are complete%
+
+%检查文件是否完整%
 if exist .\Magisk.zip (
-choice /C AB /N /M "Please select A or B:"
-if errorlevel 2 goto flash_b
-goto flash_a
+    choice /C AB /N /M "Please select A or B:"
+    if errorlevel 2 goto flash_b
+    goto flash_a
 ) else (
-goto error-noMagisk.zip
+    goto error-noMagisk.zip
 )
+
 
 :error-noMagisk.zip
 CLS
 echo.
-echo. Magisk.zip does not exist. Please check if Magisk file is placed.
+echo.  Magisk.zip does not exist. Please check if Magisk file is placed.
 pause>null
 del null
 exit
 
 :flash_a
 CLS
-bin\busybox unzip Magisk.zip -d tmp -n |bin\busybox grep -E "arm|util_functions" |bin\busybox sed "s/ //g"
+choice /C AB /N /M "Please select the Magisk branch to flash into: Magisk_Delta(A) or Magisk_topjohnwu(B)"
+if errorlevel 2 (
+    rem 选择 B，执行 flash_a_top
+    goto flash_a_top
+) else (
+    rem 选择 A，执行 flash_a
+    bin\busybox unzip Magisk.zip -d tmp -n |bin\busybox grep -E "arm|util_functions" |bin\busybox sed "s/ //g"
 echo.
-if not exist tmp/META-INF (echo.File error!&rd /s /q tmp 1>nul 2>nul&pause&exit)
+if not exist tmp/META-INF (echo.File error! &rd /s /q tmp 1>nul 2>nul&pause&exit)
 
 if exist tmp\assets\util_functions.sh bin\busybox bash -c "echo Magisk version will be updated to: $(cat tmp/assets/util_functions.sh |grep MAGISK_VER_CODE |cut -d = -f 2)"
 if exist tmp\lib\arm64-v8a (
-del bin\magisk64 1>nul 2>nul
-del bin\magiskinit 1>nul 2>nul
-copy tmp\lib\arm64-v8a\libmagisk64.so bin\magisk64 1>nul 2>nul
-copy tmp\lib\arm64-v8a\libmagiskinit.so bin\magiskinit 1>nul 2>nul
+	del bin\magisk64 1>nul 2>nul
+	del bin\magiskinit 1>nul 2>nul
+	copy tmp\lib\arm64-v8a\libmagisk64.so bin\magisk64 1>nul 2>nul
+	copy tmp\lib\arm64-v8a\libmagiskinit.so bin\magiskinit 1>nul 2>nul
 )
 if exist tmp\lib\armeabi-v7a (
-del bin\magisk32 1>nul 2>nul
-copy tmp\lib\armeabi-v7a\libmagisk32.so bin\magisk32 1>nul 2>nul
-if exist tmp\lib\armeabi-v7a\libmagisk64.so (
-copy tmp\lib\armeabi-v7a\libmagisk64.so bin\magisk64 1>nul 2>nul
-)
-if not exist tmp\lib\arm64-v8a\libmagiskinit.so (
-copy tmp\lib\armeabi-v7a\libmagiskinit.so bin\magiskinit 1>nul 2>nul
-)
+	del bin\magisk32 1>nul 2>nul
+	copy tmp\lib\armeabi-v7a\libmagisk32.so bin\magisk32 1>nul 2>nul
+	if exist tmp\lib\armeabi-v7a\libmagisk64.so (
+		copy tmp\lib\armeabi-v7a\libmagisk64.so bin\magisk64 1>nul 2>nul
+	)
+	if not exist tmp\lib\arm64-v8a\libmagiskinit.so (
+		copy tmp\lib\armeabi-v7a\libmagiskinit.so bin\magiskinit 1>nul 2>nul
+	)
 )
 echo.Update completed!
 rd /s /q tmp
@@ -76,9 +84,9 @@ timeout /t 10 >nul
 %adb-tools%\fastboot devices
 
 if errorlevel 1 (
-echo Device not detected in Fastboot!
-pause
-exit /b
+    echo Device not detected in Fastboot!
+    pause
+    exit /b
 )
 
 echo Device connected, proceeding with further operations...
@@ -87,48 +95,51 @@ echo Device connected, proceeding with further operations...
 echo Device will restart and enter the system. Enjoy!
 %adb-tools%\fastboot reboot
 goto end
+)
+
+
 
 :flash_b
 CLS
-bin\busybox unzip Magisk.zip -d tmp -n |bin\busybox grep -E "arm|util_functions" |bin\busybox sed "s/ //g"
+choice /C AB /N /M "Please select the Magisk branch to flash into: Magisk_Delta(A) or Magisk_topjohnwu(B)"
+if errorlevel 2 (
+    rem 选择 B，执行 flash_b_top
+    goto flash_b_top
+) else (
+    rem 选择 A，执行 flash_b
+    bin\busybox unzip Magisk.zip -d tmp -n |bin\busybox grep -E "arm|util_functions" |bin\busybox sed "s/ //g"
 echo.
-if not exist tmp/META-INF (echo.File error!&rd /s /q tmp 1>nul 2>nul&pause&exit)
+if not exist tmp/META-INF (echo.File error! &rd /s /q tmp 1>nul 2>nul&pause&exit)
 
 if exist tmp\assets\util_functions.sh bin\busybox bash -c "echo Magisk version will be updated to: $(cat tmp/assets/util_functions.sh |grep MAGISK_VER_CODE |cut -d = -f 2)"
 if exist tmp\lib\arm64-v8a (
-del bin\magisk64 1>nul 2>nul
-del bin\magiskinit 1>nul 2>nul
-copy tmp\lib\arm64-v8a\libmagisk64.so bin\magisk64 1>nul 2>nul
-copy tmp\lib\arm64-v8a\libmagiskinit.so bin\magiskinit 1>nul 2>nul
+	del bin\magisk64 1>nul 2>nul
+	del bin\magiskinit 1>nul 2>nul
+	copy tmp\lib\arm64-v8a\libmagisk64.so bin\magisk64 1>nul 2>nul
+	copy tmp\lib\arm64-v8a\libmagiskinit.so bin\magiskinit 1>nul 2>nul
 )
 if exist tmp\lib\armeabi-v7a (
-del bin\magisk32 1>nul 2>nul
-copy tmp\lib\armeabi-v7a\libmagisk32.so bin\magisk32 1>nul 2>nul
-if exist tmp\lib\armeabi-v7a\libmagisk64.so (
-copy tmp\lib\armeabi-v7a\libmagisk64.so bin\magisk64 1>nul 2>nul
-)
-if not exist tmp\lib\arm64-v8a\libmagiskinit.so (
-copy tmp\lib\armeabi-v7a\libmagiskinit.so bin\magiskinit 1>nul 2>nul
-)
+	del bin\magisk32 1>nul 2>nul
+	copy tmp\lib\armeabi-v7a\libmagisk32.so bin\magisk32 1>nul 2>nul
+	if exist tmp\lib\armeabi-v7a\libmagisk64.so (
+		copy tmp\lib\armeabi-v7a\libmagisk64.so bin\magisk64 1>nul 2>nul
+	)
+	if not exist tmp\lib\arm64-v8a\libmagiskinit.so (
+		copy tmp\lib\armeabi-v7a\libmagiskinit.so bin\magiskinit 1>nul 2>nul
+	)
 )
 echo.Update completed!
 rd /s /q tmp
 
-.\source\payload\payload-dumper-go.exe -p int_boot -o %boot_origin% .\payload.bin
-if not exist %boot_origin%\int_boot.img (echo.Current directory does not have int_boot.img file!& pause & exit /b)
+.\source\payload\payload-dumper-go.exe -p init_boot -o %boot_origin% .\payload.bin
+if not exist %boot_origin%\init_boot.img (echo.Current directory does not have init_boot.img file!& pause & exit /b)
 if exist %boot_Magiskpatched%\boot_Magiskpatched.img (del /s /q %boot_Magiskpatched%\boot_Magiskpatched.img 1>nul 2>nul)
 echo.
-bin\busybox bash bin/boot_patch.sh %boot_origin%\int_boot.img
+bin\busybox bash bin/boot_patch.sh %boot_origin%\init_boot.img
 echo.
 if exist new-boot.img (move new-boot.img %boot_Magiskpatched%\boot_Magiskpatched.img 1>nul 2>nul&echo.Successfully patched boot.img) else (echo.Unable to patch boot.img)
 
 %adb-tools%\adb devices | findstr /r /c:"^[ \t]*[0-9a-zA-Z]+[ \t]*device$"
-
-if errorlevel 1 (
-echo Device not detected in ADB!
-pause
-exit /b
-)
 
 echo Device connected, proceeding with further operations...
 
@@ -140,32 +151,156 @@ timeout /t 10 >nul
 %adb-tools%\fastboot bootloader
 
 if errorlevel 1 (
-echo Device not detected in Fastboot!
-pause
-exit /b
+    echo Device not detected in Fastboot!
+    pause
+    exit /b
 )
 
 echo Device connected, proceeding with further operations...
-%adb-tools%\fastboot flash int_boot %boot_Magiskpatched%\boot_Magiskpatched.img
+%adb-tools%\fastboot flash init_boot %boot_Magiskpatched%\boot_Magiskpatched.img
 
-echo Device will restart and enter the system after 3 seconds. Enjoy!
+echo Device will restart and enter the system. Enjoy!
+timeout /t 3 >nul
+%adb-tools%\fastboot reboot
+goto end
+)
+
+
+
+:flash_a_top
+CLS
+bin\busybox unzip Magisk-v26.1.zip -d tmp -n |bin\busybox grep -E "arm|util_functions" |bin\busybox sed "s/ //g"
+echo.
+if not exist tmp/META-INF (echo.File error!&rd /s /q tmp 1>nul 2>nul&pause&exit)
+
+if exist tmp\assets\util_functions.sh bin\busybox bash -c "echo Magisk version will be updated to: $(cat tmp/assets/util_functions.sh |grep MAGISK_VER_CODE |cut -d = -f 2)"
+if exist tmp\lib\arm64-v8a (
+	del bin\magisk64 1>nul 2>nul
+	del bin\magiskinit 1>nul 2>nul
+	copy tmp\lib\arm64-v8a\libmagisk64.so bin\magisk64 1>nul 2>nul
+	copy tmp\lib\arm64-v8a\libmagiskinit.so bin\magiskinit 1>nul 2>nul
+)
+if exist tmp\lib\armeabi-v7a (
+	del bin\magisk32 1>nul 2>nul
+	copy tmp\lib\armeabi-v7a\libmagisk32.so bin\magisk32 1>nul 2>nul
+	if exist tmp\lib\armeabi-v7a\libmagisk64.so (
+		copy tmp\lib\armeabi-v7a\libmagisk64.so bin\magisk64 1>nul 2>nul
+	)
+	if not exist tmp\lib\arm64-v8a\libmagiskinit.so (
+		copy tmp\lib\armeabi-v7a\libmagiskinit.so bin\magiskinit 1>nul 2>nul
+	)
+)
+echo.Update completed!
+rd /s /q tmp
+
+.\source\payload\payload-dumper-go.exe -p boot -o %boot_origin% .\payload.bin
+if not exist %boot_origin%\boot.img (echo.Current directory does not have boot.img file!& pause & exit /b)
+if exist %boot_Magiskpatched%\boot_Magiskpatched.img (del /s /q %boot_Magiskpatched%\boot_Magiskpatched.img 1>nul 2>nul)
+echo.
+bin\busybox bash bin/boot_patch.sh %boot_origin%\boot.img
+echo.
+if exist new-boot.img (move new-boot.img %boot_Magiskpatched%\boot_Magiskpatched.img 1>nul 2>nul&echo.Successfully patched boot.img) else (echo.Unable to patch boot.img)
+
+%adb-tools%\adb devices | findstr /r /c:"^[ \t]*[0-9a-zA-Z]+[ \t]*device$"
+
+echo Device connected, proceeding with further operations...
+
+%adb-tools%\adb reboot bootloader
+
+echo Waiting for 10 seconds... for the device to enter fastboot
+timeout /t 10 >nul
+
+%adb-tools%\fastboot devices
+
+if errorlevel 1 (
+    echo Device not detected in Fastboot!
+    pause
+    exit /b
+)
+
+echo Device connected, proceeding with further operations...
+%adb-tools%\fastboot flash boot %boot_Magiskpatched%\boot_Magiskpatched.img
+
+echo Device will restart and enter the system. Enjoy!
+%adb-tools%\fastboot reboot
+goto end
+
+
+:flash_b_top
+CLS
+bin\busybox unzip Magisk-V26.1.zip -d tmp -n |bin\busybox grep -E "arm|util_functions" |bin\busybox sed "s/ //g"
+echo.
+if not exist tmp/META-INF (echo.flies is error！&rd /s /q tmp 1>nul 2>nul&pause&exit)
+
+if exist tmp\assets\util_functions.sh bin\busybox bash -c "echo The magisk version will be updated to： $(cat tmp/assets/util_functions.sh |grep MAGISK_VER_CODE |cut -d = -f 2)"
+if exist tmp\lib\arm64-v8a (
+	del bin\magisk64 1>nul 2>nul
+	del bin\magiskinit 1>nul 2>nul
+	copy tmp\lib\arm64-v8a\libmagisk64.so bin\magisk64 1>nul 2>nul
+	copy tmp\lib\arm64-v8a\libmagiskinit.so bin\magiskinit 1>nul 2>nul
+)
+if exist tmp\lib\armeabi-v7a (
+	del bin\magisk32 1>nul 2>nul
+	copy tmp\lib\armeabi-v7a\libmagisk32.so bin\magisk32 1>nul 2>nul
+	if exist tmp\lib\armeabi-v7a\libmagisk64.so (
+		copy tmp\lib\armeabi-v7a\libmagisk64.so bin\magisk64 1>nul 2>nul
+	)
+	if not exist tmp\lib\arm64-v8a\libmagiskinit.so (
+		copy tmp\lib\armeabi-v7a\libmagiskinit.so bin\magiskinit 1>nul 2>nul
+	)
+)
+echo.update completed!
+rd /s /q tmp
+
+.\source\payload\payload-dumper-go.exe -p init_boot -o %boot_origin% .\payload.bin
+if not exist %boot_origin%\init_boot.img (echo.There is no init_boot.img file in the current directory！& pause & exit /b)
+if exist %boot_Magiskpatched%\boot_Magiskpatched.img (del /s /q %boot_Magiskpatched%\boot_Magiskpatched.img 1>nul 2>nul)
+echo.
+bin\busybox bash bin/boot_patch.sh %boot_origin%\init_boot.img
+echo.
+if exist new-boot.img (move new-boot.img %boot_Magiskpatched%\boot_Magiskpatched.img 1>nul 2>nul&echo.Successfully patched boot.img) else (echo.cannot be patched boot.img)
+
+%adb-tools%\adb devices | findstr /r /c:"^[ \t]*[0-9a-zA-Z]+[ \t]*device$"
+
+echo The device is connected, continue to do other operations...
+
+%adb-tools%\adb reboot fastboot
+
+echo Waiting 10 seconds... for device to enter fastboot
+timeout /t 10 >nul
+
+%adb-tools%\fastboot bootloader
+
+if errorlevel 1 (
+    echo Fastboot not detecting the device!
+    pause
+    exit /b
+)
+
+echo The device is connected, continue to do other operations...
+%adb-tools%\fastboot flash init_boot %boot_Magiskpatched%\boot_Magiskpatched.img
+
+echo The device will restart into the system after 3 seconds, I wish you a happy use~
 timeout /t 3 >nul
 %adb-tools%\fastboot reboot
 goto end
 
+
 :end
-echo.Do you want to delete the payload.bin file? (Y for delete / N for keep)
+echo.    Do you want to delete the payload.bin file? (Y to delete/N not to delete)
 choice /c YN
 
 if errorlevel 2 (
-echo Deleting residual files...
-del /s /q %boot_Magiskpatched%\boot_Magiskpatched.img
-del /s /q %boot_origin%\boot.img
+    echo Delete the flies for No
+	del /s /q %boot_Magiskpatched%\boot_Magiskpatched.img
+	del /s /q %boot_origin%\boot.img
+	del /s /q %boot_origin%\init_boot.img
 ) else (
-echo Deleting files...
-del /s /q .\payload.bin
-del /s /q %boot_Magiskpatched%\boot_Magiskpatched.img
-del /s /q %boot_origin%\boot.img
+    echo Delete the flies for Yes
+    del /s /q .\payload.bin
+	del /s /q %boot_Magiskpatched%\boot_Magiskpatched.img
+	del /s /q %boot_origin%\boot.img
+	del /s /q %boot_origin%\init_boot.img
 )
 endlocal
 
--- 开始(CN).bat
+++ 开始(CN).bat
@@ -11,7 +11,7 @@ echo.
 echo.          全自动刷入magisk delta
 echo.                               by badnng
 echo.按A键开始进行老机型的boot的全自动刷入~
-echo.按B键开始进行新机型int_boot的全自动刷入~
+echo.按B键开始进行新机型init_boot的全自动刷入~
 
 
 %检查文件是否完整%
@@ -34,7 +34,142 @@ exit
 
 :flash_a
 CLS
-bin\busybox unzip Magisk.zip -d tmp -n |bin\busybox grep -E "arm|util_functions" |bin\busybox sed "s/ //g"
+choice /C AB /N /M "请选择要刷入的Magisk分支：Magisk_Delta(A) 或 Magisk_topjohnwu(B)"
+if errorlevel 2 (
+    rem 选择 B，执行 flash_b
+    goto flash_a_top
+) else (
+    rem 选择 A，执行 flash_a
+    bin\busybox unzip Magisk.zip -d tmp -n |bin\busybox grep -E "arm|util_functions" |bin\busybox sed "s/ //g"
+echo.
+if not exist tmp/META-INF (echo.文件错误！&rd /s /q tmp 1>nul 2>nul&pause&exit)
+
+if exist tmp\assets\util_functions.sh bin\busybox bash -c "echo 面具版本将更新为： $(cat tmp/assets/util_functions.sh |grep MAGISK_VER_CODE |cut -d = -f 2)"
+if exist tmp\lib\arm64-v8a (
+	del bin\magisk64 1>nul 2>nul
+	del bin\magiskinit 1>nul 2>nul
+	copy tmp\lib\arm64-v8a\libmagisk64.so bin\magisk64 1>nul 2>nul
+	copy tmp\lib\arm64-v8a\libmagiskinit.so bin\magiskinit 1>nul 2>nul
+)
+if exist tmp\lib\armeabi-v7a (
+	del bin\magisk32 1>nul 2>nul
+	copy tmp\lib\armeabi-v7a\libmagisk32.so bin\magisk32 1>nul 2>nul
+	if exist tmp\lib\armeabi-v7a\libmagisk64.so (
+		copy tmp\lib\armeabi-v7a\libmagisk64.so bin\magisk64 1>nul 2>nul
+	)
+	if not exist tmp\lib\arm64-v8a\libmagiskinit.so (
+		copy tmp\lib\armeabi-v7a\libmagiskinit.so bin\magiskinit 1>nul 2>nul
+	)
+)
+echo.更新完成！
+rd /s /q tmp
+
+.\source\payload\payload-dumper-go.exe -p boot -o %boot_origin% .\payload.bin
+if not exist %boot_origin%\boot.img (echo.当前目录没有 boot.img 文件！& pause & exit /b)
+if exist %boot_Magiskpatched%\boot_Magiskpatched.img (del /s /q %boot_Magiskpatched%\boot_Magiskpatched.img 1>nul 2>nul)
+echo.
+bin\busybox bash bin/boot_patch.sh %boot_origin%\boot.img
+echo.
+if exist new-boot.img (move new-boot.img %boot_Magiskpatched%\boot_Magiskpatched.img 1>nul 2>nul&echo.成功修补 boot.img) else (echo.无法修补 boot.img)
+
+%adb-tools%\adb devices | findstr /r /c:"^[ \t]*[0-9a-zA-Z]+[ \t]*device$"
+
+echo 设备已连接，继续执行其他操作...
+
+%adb-tools%\adb reboot bootloader
+
+echo 正在等待 10 秒...以便设备能进入fastboot
+timeout /t 10 >nul
+
+%adb-tools%\fastboot devices
+
+if errorlevel 1 (
+    echo Fastboot未检测到设备！
+    pause
+    exit /b
+)
+
+echo 设备已连接，继续执行其他操作...
+%adb-tools%\fastboot flash boot %boot_Magiskpatched%\boot_Magiskpatched.img
+
+echo 设备将重启进入系统，祝您使用愉快~
+%adb-tools%\fastboot reboot
+goto end
+)
+
+
+
+:flash_b
+CLS
+choice /C AB /N /M "请选择要刷入的Magisk分支：Magisk_Delta(A) 或 Magisk_topjohnwu(B)"
+if errorlevel 2 (
+    rem 选择 B，执行 flash_b_top
+    goto flash_b_top
+) else (
+    rem 选择 A，执行 flash_b
+    bin\busybox unzip Magisk.zip -d tmp -n |bin\busybox grep -E "arm|util_functions" |bin\busybox sed "s/ //g"
+echo.
+if not exist tmp/META-INF (echo.文件错误！&rd /s /q tmp 1>nul 2>nul&pause&exit)
+
+if exist tmp\assets\util_functions.sh bin\busybox bash -c "echo 面具版本将更新为： $(cat tmp/assets/util_functions.sh |grep MAGISK_VER_CODE |cut -d = -f 2)"
+if exist tmp\lib\arm64-v8a (
+	del bin\magisk64 1>nul 2>nul
+	del bin\magiskinit 1>nul 2>nul
+	copy tmp\lib\arm64-v8a\libmagisk64.so bin\magisk64 1>nul 2>nul
+	copy tmp\lib\arm64-v8a\libmagiskinit.so bin\magiskinit 1>nul 2>nul
+)
+if exist tmp\lib\armeabi-v7a (
+	del bin\magisk32 1>nul 2>nul
+	copy tmp\lib\armeabi-v7a\libmagisk32.so bin\magisk32 1>nul 2>nul
+	if exist tmp\lib\armeabi-v7a\libmagisk64.so (
+		copy tmp\lib\armeabi-v7a\libmagisk64.so bin\magisk64 1>nul 2>nul
+	)
+	if not exist tmp\lib\arm64-v8a\libmagiskinit.so (
+		copy tmp\lib\armeabi-v7a\libmagiskinit.so bin\magiskinit 1>nul 2>nul
+	)
+)
+echo.更新完成！
+rd /s /q tmp
+
+.\source\payload\payload-dumper-go.exe -p init_boot -o %boot_origin% .\payload.bin
+if not exist %boot_origin%\init_boot.img (echo.当前目录没有 init_boot.img 文件！& pause & exit /b)
+if exist %boot_Magiskpatched%\boot_Magiskpatched.img (del /s /q %boot_Magiskpatched%\boot_Magiskpatched.img 1>nul 2>nul)
+echo.
+bin\busybox bash bin/boot_patch.sh %boot_origin%\init_boot.img
+echo.
+if exist new-boot.img (move new-boot.img %boot_Magiskpatched%\boot_Magiskpatched.img 1>nul 2>nul&echo.成功修补 boot.img) else (echo.无法修补 boot.img)
+
+%adb-tools%\adb devices | findstr /r /c:"^[ \t]*[0-9a-zA-Z]+[ \t]*device$"
+
+echo 设备已连接，继续执行其他操作...
+
+%adb-tools%\adb reboot fastboot
+
+echo 正在等待 10 秒...以便设备能进入fastboot
+timeout /t 10 >nul
+
+%adb-tools%\fastboot bootloader
+
+if errorlevel 1 (
+    echo Fastboot未检测到设备！
+    pause
+    exit /b
+)
+
+echo 设备已连接，继续执行其他操作...
+%adb-tools%\fastboot flash init_boot %boot_Magiskpatched%\boot_Magiskpatched.img
+
+echo 设备将在3秒后重启进入系统，祝您使用愉快~
+timeout /t 3 >nul
+%adb-tools%\fastboot reboot
+goto end
+)
+
+
+
+:flash_a_top
+CLS
+bin\busybox unzip Magisk-v26.1.zip -d tmp -n |bin\busybox grep -E "arm|util_functions" |bin\busybox sed "s/ //g"
 echo.
 if not exist tmp/META-INF (echo.文件错误！&rd /s /q tmp 1>nul 2>nul&pause&exit)
 
@@ -91,9 +226,9 @@ echo 设备将重启进入系统，祝您使用愉快~
 goto end
 
 
-:flash_b
+:flash_b_top
 CLS
-bin\busybox unzip Magisk.zip -d tmp -n |bin\busybox grep -E "arm|util_functions" |bin\busybox sed "s/ //g"
+bin\busybox unzip Magisk-V26.1.zip -d tmp -n |bin\busybox grep -E "arm|util_functions" |bin\busybox sed "s/ //g"
 echo.
 if not exist tmp/META-INF (echo.文件错误！&rd /s /q tmp 1>nul 2>nul&pause&exit)
 
@@ -117,21 +252,15 @@ if exist tmp\lib\armeabi-v7a (
 echo.更新完成！
 rd /s /q tmp
 
-.\source\payload\payload-dumper-go.exe -p int_boot -o %boot_origin% .\payload.bin
-if not exist %boot_origin%\int_boot.img (echo.当前目录没有 int_boot.img 文件！& pause & exit /b)
+.\source\payload\payload-dumper-go.exe -p init_boot -o %boot_origin% .\payload.bin
+if not exist %boot_origin%\init_boot.img (echo.当前目录没有 init_boot.img 文件！& pause & exit /b)
 if exist %boot_Magiskpatched%\boot_Magiskpatched.img (del /s /q %boot_Magiskpatched%\boot_Magiskpatched.img 1>nul 2>nul)
 echo.
-bin\busybox bash bin/boot_patch.sh %boot_origin%\int_boot.img
+bin\busybox bash bin/boot_patch.sh %boot_origin%\init_boot.img
 echo.
 if exist new-boot.img (move new-boot.img %boot_Magiskpatched%\boot_Magiskpatched.img 1>nul 2>nul&echo.成功修补 boot.img) else (echo.无法修补 boot.img)
 
 %adb-tools%\adb devices | findstr /r /c:"^[ \t]*[0-9a-zA-Z]+[ \t]*device$"
-
-if errorlevel 1 (
-    echo ADB未检测到设备！
-    pause
-    exit /b
-)
 
 echo 设备已连接，继续执行其他操作...
 
@@ -149,7 +278,7 @@ if errorlevel 1 (
 )
 
 echo 设备已连接，继续执行其他操作...
-%adb-tools%\fastboot flash int_boot %boot_Magiskpatched%\boot_Magiskpatched.img
+%adb-tools%\fastboot flash init_boot %boot_Magiskpatched%\boot_Magiskpatched.img
 
 echo 设备将在3秒后重启进入系统，祝您使用愉快~
 timeout /t 3 >nul
@@ -165,11 +294,13 @@ if errorlevel 2 (
     echo 正在删除残留文件
 	del /s /q %boot_Magiskpatched%\boot_Magiskpatched.img
 	del /s /q %boot_origin%\boot.img
+	del /s /q %boot_origin%\init_boot.img
 ) else (
     echo 删除文件
     del /s /q .\payload.bin
 	del /s /q %boot_Magiskpatched%\boot_Magiskpatched.img
 	del /s /q %boot_origin%\boot.img
+	del /s /q %boot_origin%\init_boot.img
 )
 endlocal
 
