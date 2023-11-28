1739e780aaffc638aff26011503d24695519ecf2
badnng <wsj316@outlook.com>
2023-11-29 01:09:24 +0800

    Fix the init_boot flash bugs

--- 开始.bat
+++ 开始.bat
@@ -17,8 +17,8 @@ title 全自动刷入magisk_V2---by badnng
 echo.
 echo.          全自动刷入magisk_V2
 echo.                               by badnng
-echo.按A键开始进行老机型的boot的全自动刷入~
-echo.按B键开始进行新机型init_boot的全自动刷入~
+echo.按A键开始进行内核版本小于5.15版本的boot全自动刷入~
+echo.按B键开始进行内核版本大于或等于5.15版本的init_boot全自动刷入~
 
 :Nopatch_flies
 echo.
@@ -36,6 +36,8 @@ if exist %Magisk_source%\magisk_lib.zip (
 
 :flash_boot
 CLS
+echo. 正在检测授权，如未授权，会卡在这不动或报错，请知晓
+%adb-tools%\adb devices
 echo. 安装Magisk，如安装失败，请确保是否给电脑授权usb安装或系统管家拦截（如MIUI，HyperOS）
 %adb-tools%\adb install %Magisk_flies%/Magisk.apk
 echo. 解压所需文件
@@ -64,10 +66,12 @@ goto end
 
 :flash_initboot
 CLS
+echo. 正在检测授权，如未授权，会卡在这不动或报错，请知晓
+%adb-tools%\adb devices
 echo. 安装Magisk，如安装失败，请确保是否给电脑授权usb安装或系统管家拦截（如MIUI，HyperOS）
-adb install %Magisk_flies%/Magisk.apk
+%adb-tools%\adb install %Magisk_flies%/Magisk.apk
 echo. 解压所需文件
-tar -xzvf %Magisk_source%/magisk_lib.zip -C %Magisk_source% && REM 解压magisk-lib文件
+.\source\7zip\7z x .\source\Magisk_flies\magisk_lib.zip -o.\source\Magisk_flies && REM 解压magisk-lib文件
 echo. 修补并提取boot
 %payload%\payload-dumper-go.exe -p init_boot -o %boot_origin% %payload_file%
 %adb-tools%\adb push .\source\Magisk_flies\Magisk\ /data/local/tmp && REM 推送脚本
